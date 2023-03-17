58949cdc1197ebe4cd345f09c28911094893a6de
Mr-Wallace <unifystudiosinc@gmail.com>
2023-03-16 19:15:43 -0700

    success

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ int main(void)
 	long int b;
 	long long int c;
 	char d;
-	float f;i
+	float f;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
